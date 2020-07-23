f98c22e75606f5cc2dca8077371da19542499920
Leonardo Vizagre <leonardo.vizagre@gmail.com>
2020-07-22 22:27:54 -0300

    Explorando a sintaxe do c#

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,14 +7,34 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            if(args.Length > 0) {
-                Console.WriteLine("Hello, " + args[0] + "!");
-                Console.WriteLine($"Hello, {args[0]}!");
-            } 
-            else
+            // Array de valores double
+            var numbers = new[] {12.7, 10.3, 6.11, 4.1};
+
+            var result_0 = 0.0;
+            foreach(var number in numbers)
             {
-                Console.WriteLine("Hello!");
+                result_0 += number;
             }
+
+            Console.WriteLine($"Soma dos valores do array: {result_0}");
+
+            // Lista de valores double
+            var grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
+            grades.Add(56.1);
+
+            var result_1 = 0.0;
+            var media = 0.0;
+            foreach(var grade in grades)
+            {
+                result_1 += grade;
+            }
+
+            double v = result_1 / grades.Count;
+            media = v;
+
+            Console.WriteLine($"Soma dos valores da lista: {result_1}");
+            Console.WriteLine($"Média dos valores da lista: {media:N1}");
+
         }
     }
 }
